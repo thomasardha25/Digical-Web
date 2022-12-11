d10f403ad7bfdcdbaeac27f5ed0f5e03e97d69c5
thomasardha25 <75558449+thomasardha25@users.noreply.github.com>
2022-12-11 22:03:48 +0700

    Add files via upload

--- database/digical_pabweb.sql
+++ database/digical_pabweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2022 at 09:16 PM
+-- Generation Time: Dec 11, 2022 at 04:02 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -41,7 +41,7 @@ CREATE TABLE `gejala_penyakit` (
 --
 
 INSERT INTO `gejala_penyakit` (`id`, `nama_penyakit`, `sub_judul1`, `pengertian`, `sub_judul2`, `gejala`) VALUES
-(1, 'Umpan Lambung', 'Pengertian Umpan Lambung', 'Penyakit umpan lambung atau gastroesophageal reflux disease (GERD) adalah kondisi ketika asam lambung naik ke esofagus atau kerongkongan. Kondisi ini dapat menimbulkan nyeri pada ulu hati, heartburn, serta berbagai gejala lainnya pada area dada bagian bawah dan perut.', 'Gejala Umpan Lambung', 'Batuk kering secara terus-menerus di malam hari\n</br>\nSakit pada bagian dada\n</br>\nKesulitan pernapasan\n</br>\nTenggorokan serak\n</br>\nBau mulut'),
+(1, 'Asam Lambung', 'Pengertian Asam Lambung', 'Penyakit asam lambung atau gastroesophageal reflux disease (GERD) adalah kondisi ketika asam lambung naik ke esofagus atau kerongkongan. Kondisi ini dapat menimbulkan nyeri pada ulu hati, heartburn, serta berbagai gejala lainnya pada area dada bagian bawah dan perut.', 'Gejala Asam Lambung', 'Batuk kering secara terus-menerus di malam hari\n</br>\nSakit pada bagian dada\n</br>\nKesulitan pernapasan\n</br>\nTenggorokan serak\n</br>\nBau mulut'),
 (2, 'Asma', 'Pengertian Asma', 'Asma adalah salah satu masalah paru-paru yang membuat pengidapnya kesulitan bernapas akibat peradangan dan penyempitan pada saluran pernapasan.', 'Gejala Asma', 'Sesak dada\r\n<br>\r\nBatuk\r\n<br>\r\nSesak napas\r\n<br>\r\nMengi, yang menyebabkan suara siulan saat mengeluarkan napas');
 
 -- --------------------------------------------------------
@@ -62,15 +62,44 @@ CREATE TABLE `tb_pasien` (
 --
 
 INSERT INTO `tb_pasien` (`id`, `nama`, `penyakit`, `alamat`) VALUES
-(1, 'denis', 'asma', 'bandung'),
-(2, 'thomas', 'batuk', 'jakarta'),
 (3, 'ardhan', 'maag', 'tegal'),
 (4, 'Templeton', 'batuk', 'bogor'),
 (5, 'ringo', 'asma', 'bandung'),
 (7, 'Tom Morello', 'batuk', 'jakarta'),
 (8, 'anda', 'maag', 'depok'),
 (9, 'Malin', 'durhaka', 'padang\r\n'),
-(10, 'Paul', 'maag', 'bogor');
+(10, 'Paul', 'maag', 'bogor'),
+(12, 'danu', 'diare', 'bekasi'),
+(13, 'zidan', 'asma', 'jakarta'),
+(16, 'cd', 'lambung', 'Jakarta'),
+(17, 'ef', 'migrain', 'Tegal'),
+(18, 'ab', 'amandel', 'Bandung'),
+(19, 'cd', 'lambung', 'Jakarta'),
+(20, 'ef', 'migrain', 'Tegal'),
+(21, 'Andani', 'Flu', 'Malang'),
+(22, 'Amelia', 'Demam', 'Surabaya'),
+(23, 'Desta', 'Batuk', 'Bogor'),
+(24, 'sd', 'sda', 'as'),
+(25, 'fds', 'fad', 'ads'),
+(26, 'Denis', 'Batuk', 'Bandung'),
+(27, 'Thomas', 'Batuk', 'Jakarta'),
+(28, 'Ardhan', 'Maag', 'Tegal'),
+(29, 'Templeton', 'Batuk', 'Bogor'),
+(30, 'Ringo', 'Asma', 'Bandung'),
+(31, 'Tom Morello', 'Batuk', 'Jakarta'),
+(32, 'Anda', 'Maag', 'Depok'),
+(33, 'Malin', 'Durhaka', 'Padang\n'),
+(34, 'Paul', 'Maag', 'Bogor'),
+(35, 'Danu', 'Diare', 'Bekasi'),
+(36, 'Zidan', 'Asma', 'Jakarta'),
+(37, 'Bowo', 'Usus Buntu', 'Cirebon'),
+(38, 'Ab', 'Amandel', 'Bandung'),
+(39, 'Cd', 'Lambung', 'Jakarta'),
+(40, 'Ef', 'Migrain', 'Tegal'),
+(41, 'sd', 'sda', 'as'),
+(42, 'fds', 'fad', 'ads'),
+(43, 'sd', 'sda', 'as'),
+(44, 'fds', 'fad', 'ads');
 
 -- --------------------------------------------------------
 
@@ -94,11 +123,18 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(3, 'Timothy Ardhaneswara', 'timothy.ardhanes@gmail.com', 'default.png', '$2y$10$z59Ow7h5KHJcKKfpVI9mu.ax429DQejSU3fykup9xMRIIews7a5VG', 2, 1, 1668609596),
-(4, 'Timothy Ardhaneswara', 'thomas.ardhanes@gmail.com', 'default.png', '$2y$10$BqXB0Q/4Pg8nEk7cRshaIuQxrtya2/mtQ4WciMdYJfwDRZzz7gSga', 1, 1, 1668609995),
+(3, 'Timothy Ardhaneswara', 'timothy.ardhanes@gmail.com', 'WhatsApp_Image_2022-07-03_at_00_52_20.jpeg', '$2y$10$z59Ow7h5KHJcKKfpVI9mu.ax429DQejSU3fykup9xMRIIews7a5VG', 2, 1, 1668609596),
+(4, 'Timothy Ardhaneswara', 'admin@gmail.com', 'tes.jpeg', '$2y$10$BqXB0Q/4Pg8nEk7cRshaIuQxrtya2/mtQ4WciMdYJfwDRZzz7gSga', 1, 1, 1668609995),
 (5, 'Timothy Ardhaneswara', 'tardhaneswara@gmail.com', 'default.png', '$2y$10$zHkdkoxpcNTozbeyAfueUeVRGU8DWMmUMUPWx4RhmBT9leqphcwxm', 2, 1, 1668640664),
 (6, 'Timothy Ardhaneswara', 'ardhatimothy@gmail.com', 'default.png', '$2y$10$KDvpMLFlHV473f42kLAXWe58PB0l9JjCXsDTAlYfmM541B4cnpHPm', 2, 1, 1669015268),
-(7, 'Timothy Ardhaneswara', 'ardhatimothy@apps.ipb.ac.id', 'default.png', '$2y$10$3Wnd1ZD3eUrT64qcG4PR4OJiMdiVK.sQCr6FVBLOKc.tkUrppdKTW', 2, 1, 1669015545);
+(7, 'Timothy Ardhaneswara', 'ardhatimothy@apps.ipb.ac.id', 'default.png', '$2y$10$3Wnd1ZD3eUrT64qcG4PR4OJiMdiVK.sQCr6FVBLOKc.tkUrppdKTW', 2, 1, 1669015545),
+(8, 'Ardhanes Timothy', 'atom@gmail.com', 'default.png', '$2y$10$XM6hKgHjDRwjt..WpcQkhOLUEyy5/d8iB/rg7eFBxbxWmbsKpEME.', 2, 1, 1669742079),
+(12, 'Muhammad Abdullah Alwahdi', 'abdullah@gmail.com', 'abdullah.jpeg', '$2y$10$VcbaaKShBsTNCbw0c/j/GeGKc6xBf/jSnRhHe9k5cxtj4hYIzvvSS', 3, 1, 1669794877),
+(13, 'Subkhan Avina Dewanto', 'subkhan@gmail.com', 'subkhan.jpg', '$2y$10$qVbtyLURdSqsskcIZUykVOGs7eEyctZfYzJp8pJdOtnfmIlInFPAK', 3, 1, 1669794897),
+(14, 'Timothy Ardhaneswara', 'timothy@gmail.com', 'timothy.jpeg', '$2y$10$T/56HA3xwgTGf5JnH.Oowe4NPlOUrgs7syW8fQA3eVdC2pp1cQ2U.', 3, 1, 1669794917),
+(15, 'abdullah', 'abdul@gmail.com', 'default.png', '$2y$10$VTMWLFWrNLdk69LvZ2o9meVWm4MEWHP0fTzQSZ6S1NgG4OmvbSl2G', 2, 1, 1670293602),
+(16, 'Timothy Ardhaneswara', 't@gmail.co', 'default.png', '$2y$10$k6syQeK6w55YCAWRY5XtZOXcAGVJE68Q/9BEMccI.n1Yjrm.fG5JC', 2, 1, 1670596920),
+(17, 'Timothy Ardhaneswara', 'ardhanes@gmail.com', 'default.png', '$2y$10$xNLMIwvGYnUT6RwuwXDEGuH7gEiLN07plp2Tf68VyHXUkMGnNjxhe', 2, 1, 1670596972);
 
 -- --------------------------------------------------------
 
@@ -120,7 +156,8 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (1, 1, 1),
 (3, 2, 2),
 (4, 1, 3),
-(6, 1, 2);
+(6, 1, 2),
+(8, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -159,7 +196,8 @@ CREATE TABLE `user_role` (
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
-(2, 'Member\r\n');
+(2, 'Member\r\n'),
+(3, 'Dokter');
 
 -- --------------------------------------------------------
 
@@ -250,19 +288,19 @@ ALTER TABLE `gejala_penyakit`
 -- AUTO_INCREMENT for table `tb_pasien`
 --
 ALTER TABLE `tb_pasien`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user_menu`
@@ -274,7 +312,7 @@ ALTER TABLE `user_menu`
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_sub_menu`
